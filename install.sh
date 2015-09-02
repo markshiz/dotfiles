9b555340ea462dad8d5feda4843d7dfaf7f9ccd4
Mark Schisler <mark@mobconverge.com>
2015-09-01 22:20:10 -0400

    adding .tmux.conf

--- install.sh
+++ install.sh
@@ -2,7 +2,10 @@
 
 brew install bash-completion
 brew install resty
+pip install logcat-color
 rm -rf ~/.bash_profile
+rm -rf ~/.tmux.conf
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 
