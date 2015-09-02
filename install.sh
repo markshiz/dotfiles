86be526f9fa8d6aed37d41c11e74ee4d576b820d
Mark Schisler <mark.schisler@transloc.com>
2015-09-02 11:43:00 -0400

    adding bpython

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 git submodule update --init --recursive
 brew install bash-completion
 brew install resty
+pip install bpython
 rm -rf ~/.bash_profile
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 
