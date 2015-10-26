2c5c2a32628a5088a70189ebf4ba6039df75ef9e
Mark Schisler <mark.schisler@transloc.com>
2015-10-26 17:16:13 -0400

    add ropevim

--- install.sh
+++ install.sh
@@ -7,9 +7,11 @@ brew install python
 pip install --upgrade pip
 pip install bpython
 pip install logcat-color
+pip install ropevim
 rm -rf ~/.bash_profile
 rm -rf ~/.tmux.conf
+rm -rf ~/.pythonrc.py
 ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-
+ln -s ~/.dotfiles/.pythonrc.py ~/.pythonrc.py
 
