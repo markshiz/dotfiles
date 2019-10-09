4a62222a75ddaf480308320a1fb5dc0b52cc8e90
Mark Schisler <mark.schisler@knowink.com>
2019-10-08 20:13:31 -0500

    moving to zsh with catalina

--- install.sh
+++ install.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
-
+#!/bin/zsh
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 xcode-select --install
 git submodule update --init --recursive
 brew install macvim
-brew install bash-completion
 brew install heroku
 brew install python
 brew install go
@@ -19,13 +17,13 @@ sudo pip install logcat-color
 sudo pip install flake8
 sudo pip install pylint
 source powerline_fonts/install.sh
-curl https://sdk.cloud.google.com | bash
-rm -rf ~/.bash_profile
+curl https://sdk.cloud.google.com | zsh
+rm -rf ~/.zshrc
 rm -rf ~/.tmux.conf
 rm -rf ~/.pythonrc.py
-ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.pythonrc.py ~/.pythonrc.py
-\curl -sSL https://get.rvm.io | bash -s stable
+\curl -sSL https://get.rvm.io | zsh -s stable
 sudo mkdir -p /c/
 sudo chmod a+rwx /c/
